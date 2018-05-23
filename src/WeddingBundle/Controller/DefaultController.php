a88ecfb55fc2807e30a36b9b527f1f88227df1fd
David Martínez Gilo <davidmgilo@gmail.com>
2018-05-23 18:05:12 +0200

    first commit

---
+++ src/WeddingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WeddingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WeddingBundle:Default:index.html.twig');
+    }
+}
